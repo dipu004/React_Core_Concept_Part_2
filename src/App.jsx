25ae3db9a25c52ab6a7057ff45178fce436f3168
Md <dipum0505@gmail.com>
2025-03-24 23:27:19 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Batsman from './Batsman'
 import Counter from './Counter'
+import Users from './Users'
+import Friends from './Friends'
+import Posts from './Posts'
+import Players from './Players'
+
+
+const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users")
+.then(res => res.json())
+
+const fetchFriends = async() => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  return res.json();
+}
+
+const fetchPosts = async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    return res.json();
+}
+
 
 function App() {
+
+const FriendsPromise = fetchFriends()
+
+const postPromise = fetchPosts();
+
 
 function handleClick(){
   alert('Iam clicked')
@@ -17,12 +42,29 @@ function handleClick(){
 
   return (
     <>
-   
+       
+     
+
       <h3>Vite + React</h3> 
        <Batsman></Batsman>
        <Counter></Counter>
-      
+       <Players></Players>
+         
+       <Suspense fallback={<h4>Posts are coming soon!</h4>}>
+       <Posts postPromise={postPromise}></Posts>
+       </Suspense>
 
+
+      <Suspense fallback={<h3>Loading.....</h3>}>
+        <Users fetchUsers={fetchUsers}></Users>
+      </Suspense>
+
+
+      <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+      <Friends friendsPromise={FriendsPromise}></Friends>
+      </Suspense>
+
+  
      <button onClick={handleClick}>Click me!</button>
      <button onClick={function handleClick2(){
        alert('clicked2')
---
+++ src/Users.jsx
@@ -0,0 +1,12 @@
+import { use } from "react"
+
+
+export default function Users({fetchUsers}){
+    const Users = use(fetchUsers)
+      console.log(Users);
+    return(
+        <div className="card">
+            <h3>Users:{Users.length} </h3>
+        </div>
+    )
+}
